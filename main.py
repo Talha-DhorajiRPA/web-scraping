815ec771e22290b778a635e644df3ea14afa8413
TALHA <93612154+Talha-DhorajiRPA@users.noreply.github.com>
2022-01-26 21:36:34 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+
+# Open Webpage on ChromeDriver
+driver.get('htt0ps://www.olx.com.pk/mobile-phones_c1453')
+
+time.sleep(2)
+
+# Finding Element by Xpath
+link = driver.find_element_by_xpath("(//div[@class='ee2b0479'])[1]")
+
+#Click()
+link.click()
+
+time.sleep(2)
+#to go Backward
+
+driver.back()
+
+time.sleep(2)
+#to go Forward
+
+driver.forward()
+
+time.sleep(2)
+
+# Close ChromeDriver
+driver.close()
+
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,7 +45,66 @@ while flag:
     try:
         btn = driver.find_element_by_xpath("//a[@rel='next']")
         btn.click()
-        time.sleep(5)
+        time.sleep(2)
 
     except:
         flag = False
+
+#Send Keys
+
+# input = driver.find_element_by_xpath("//input[@id='home-query']")
+# input.send_keys("Suzuki")
+
+
+time.sleep(5)
+# Waits (Implicit Wait)
+# driver.implicitly_wait(20)
+# price = driver.find_element_by_xpath("//span[@class='_56dab877']")
+# print(price.text)
+
+# Wait Explicit
+# wait = WebDriverWait(driver, 10)
+# price = wait.until(
+#         EC.presence_of_element_located((By.XPATH, "//span[@class='_56dab877']"))
+#     ).text
+#
+# print(price)
+
+
+
+# time.sleep(2)
+
+# Finding Element by Xpath
+# link = driver.find_element_by_xpath("(//div[@class='ee2b0479'])[1]")
+
+#Click()
+# link.click()
+
+#get Attribute
+# imgSrc = driver.find_element_by_xpath("(//img[@class='_5b8e3f79'])[1]").get_attribute('src')
+# print(imgSrc)
+
+# imgSrc.click()
+
+# Find ELements By Xpath
+# imgSrc = driver.find_elements_by_xpath("//img[@class='_5b8e3f79']")
+# for i in imgSrc:
+#     print(i.get_attribute('src'))
+
+# time.sleep(2)
+#to go Backward
+
+# driver.back()
+
+# time.sleep(2)
+#to go Forward
+
+# driver.forward()
+
+# time.sleep(2)
+# close chromedriver
+
+
+
+
+
